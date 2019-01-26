a9d57283c0386481e91d0bccbf33b7415cb1db3d
df4517 <df4517@imperial.ac.uk>
2019-01-26 16:19:38 +0000

    df4517: Added method bodies for User and SARAdmin.

--- dronesar-backend/src/main/java/com/droneSAR/backend/SARAdmin.java
+++ dronesar-backend/src/main/java/com/droneSAR/backend/SARAdmin.java
@@ -2,4 +2,8 @@ package com.droneSAR.backend;
 
 public class SARAdmin extends User {
 
+    public SARAdmin(int userId) {
+        super(userId);
+    }
+
 }
--- dronesar-backend/src/main/java/com/droneSAR/backend/User.java
+++ dronesar-backend/src/main/java/com/droneSAR/backend/User.java
@@ -2,4 +2,36 @@ package com.droneSAR.backend;
 
 public class User {
 
+    private final int userId;
+    private Footage footage;
+
+    public User(int userId) {
+        this.userId = userId;
+    }
+
+    public int getUserId() {
+        return this.userId;
+    }
+
+    public void viewFootage() {
+        // TODO:
+    }
+
+    public void playFootage() {
+        // TODO:
+    }
+
+    public void pauseFootage() {
+        // TODO:
+    }
+
+    public void requestNewFootage() {
+        // TODO:
+    }
+
+    public void flagFootage(RESCUE_PRIORITY rescue_priority) {
+        // TODO:
+    }
+
+
 }
